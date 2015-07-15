c93822ca06faf6ec2a01ec207b4a6e565b65de17
Jason Owen <jason.a.owen@gmail.com>
2015-07-14 22:30:53 -0700

    parse command response
    Separate out the different elements of the television's response when a
    command string is sent, and print them out to demonstrate.

--- tv-serial.c
+++ tv-serial.c
@@ -18,18 +18,37 @@ void write_command(int fd) {
   ssize_t written;
 
   written = write(fd, cmd, strlen(cmd));
-  printf("Wrote command: %s  %zi of %zi bytes written.\n", cmd, written, strlen(cmd));
+  printf("Wrote command: \n%s\n  %zi of %zi bytes written.\n", cmd, written, strlen(cmd));
+}
+
+void parse_response(ssize_t len, char buf[]) {
+  char cmd;
+  int television_id;
+  char status[2];
+  int data;
+  int scan_result;
+  scan_result = sscanf(buf, " %c %d %2c%dx ", &cmd, &television_id, status, &data);
+  if (4 == scan_result) {
+    printf("Command: %c\n", cmd);
+    printf("Television ID: %i\n", television_id);
+    printf("Status: %.2s\n", status);
+    printf("Data: %i", data);
+  } else {
+    perror("error parsing result string");
+  }
+
 }
 
 void read_result(int fd) {
   ssize_t readc, i;
-  char buf[256];
-  memset(&buf, 0, 256);
-  readc = read(fd, &buf, 255);
+  char buf[32];
+  memset(&buf, 0, sizeof(buf));
+  readc = read(fd, &buf, sizeof(buf)-1);
   printf("Read %zi bytes of response: %s\n", readc, buf);
   for (i = 0; i < readc; i++) {
     printf("Byte %zi: %i (%c)\n", i, (int)buf[i], buf[i]);
   }
+  parse_response(readc, buf);
 }
 
 int main() {
