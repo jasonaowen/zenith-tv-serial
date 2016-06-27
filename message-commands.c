73fbed183da36d59faaaa1905a2cf07c45731dd8
Jason Owen <jason.a.owen@gmail.com>
2016-06-26 17:28:17 -0700

    extract get power command to new function

--- message-commands.c
+++ message-commands.c
@@ -2,6 +2,22 @@
 #include "message-commands.h"
 #include "serial-commands.h"
 
+void handle_get_power_command(CommandResult *reply,
+                              uint8_t *reply_buffer,
+                              size_t reply_buffer_length,
+                              int fd
+                             ) {
+  COMMAND_STATUS command_status;
+  POWER power;
+
+  command_status = tv_power_status(fd, &power);
+  reply->command_status = command_status;
+  if (command_status == SUCCESS) {
+    reply->has_power_state = 1;
+    reply->power_state = power;
+  }
+}
+
 size_t execute_command(Command *command, uint8_t *reply_buffer, size_t reply_buffer_length, int fd) {
   POWER power;
   INPUT input;
@@ -11,12 +27,7 @@ size_t execute_command(Command *command, uint8_t *reply_buffer, size_t reply_buf
   reply.command_status = COMMAND_STATUS__INVALID;
 
   if (command->get_power) {
-    command_status = tv_power_status(fd, &power);
-    reply.command_status = command_status;
-    if (command_status == SUCCESS) {
-      reply.has_power_state = 1;
-      reply.power_state = power;
-    }
+    handle_get_power_command(&reply, reply_buffer, reply_buffer_length, fd);
   }
 
   reply_length = command_result__get_packed_size(&reply);
