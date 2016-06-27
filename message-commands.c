6c56e777c4e99a1367cc76f5827971ce99e1e94c
Jason Owen <jason.a.owen@gmail.com>
2016-06-26 17:52:54 -0700

    add set power command to client and server

--- message-commands.c
+++ message-commands.c
@@ -18,6 +18,23 @@ void handle_get_power_command(CommandResult *reply,
   }
 }
 
+void handle_set_power_command(PowerState power_state,
+                              CommandResult *reply,
+                              uint8_t *reply_buffer,
+                              size_t reply_buffer_length,
+                              int fd
+                             ) {
+  COMMAND_STATUS command_status;
+  POWER power;
+
+  if (power_state == POWER_STATE__POWER_ON) {
+    command_status = tv_power_on(fd);
+  } else {
+    command_status = tv_power_off(fd);
+  }
+  reply->command_status = command_status;
+}
+
 size_t execute_command(Command *command, uint8_t *reply_buffer, size_t reply_buffer_length, int fd) {
   POWER power;
   INPUT input;
@@ -28,6 +45,14 @@ size_t execute_command(Command *command, uint8_t *reply_buffer, size_t reply_buf
 
   if (command->get_power) {
     handle_get_power_command(&reply, reply_buffer, reply_buffer_length, fd);
+  } else if (command->set_power) {
+    handle_set_power_command(
+      command->set_power->power_state,
+      &reply,
+      reply_buffer,
+      reply_buffer_length,
+      fd
+    );
   }
 
   reply_length = command_result__get_packed_size(&reply);
--- tv-client.py
+++ tv-client.py
@@ -1,20 +1,63 @@
-import zmq
+#!/usr/bin/env python
+
+import argparse
 import commands_pb2
+import sys
+import zmq
 
-command = commands_pb2.Command()
-command.get_power.SetInParent()
 
-context = zmq.Context()
+def parse_args():
+    description = (
+            'Send a command to the daemon controlling the television '
+            'and print its reponse.')
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('command', choices=['power'], help='Command to send')
+    parser.add_argument('argument', choices=['on', 'off', 'status'])
+    parser.add_argument('--verbose', '-v', action='count', help='Log more details. Only one level currently.')
+    return parser.parse_args()
 
-print("Connecting to server...")
-socket = context.socket(zmq.REQ)
-socket.connect("ipc:///tmp/tv-serial.sock")
+def connect_to_server():
+    print("Connecting to server...")
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("ipc:///tmp/tv-serial.sock")
+    return socket
 
-socket.send(
-  command.SerializeToString()
-)
 
-raw_result = socket.recv()
-command_result = commands_pb2.CommandResult()
-command_result.ParseFromString(raw_result)
-print("Received result: {}".format(command_result))
+def build_command_message(command, argument):
+    print("Building message for command {} with arg {}".format(command, argument))
+    message = commands_pb2.Command()
+    if command == 'power':
+        print "found `power`"
+        if argument == 'status':
+            print "found `power status`"
+            message.get_power.SetInParent()
+        elif argument == 'on':
+            message.set_power.SetInParent()
+            message.set_power.power_state = commands_pb2.POWER_ON
+        elif argument == 'off':
+            message.set_power.SetInParent()
+            message.set_power.power_state = commands_pb2.POWER_OFF
+    return message
+
+
+def print_result(raw_result):
+    command_result = commands_pb2.CommandResult()
+    command_result.ParseFromString(raw_result)
+    print("Received result: {}".format(command_result))
+
+
+def main():
+    args = parse_args()
+    command_message = build_command_message(args.command, args.argument)
+    socket = connect_to_server()
+    print("Sending message: {}".format(command_message))
+    socket.send(
+        command_message.SerializeToString()
+    )
+    print_result(socket.recv())
+
+
+if __name__ == "__main__":
+    status = main()
+    sys.exit(status)
